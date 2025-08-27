0cf403be769a3b6958b4a23dab3b1f0d8d6b3949
Max <max.wolts@gmail.com>
2025-08-27 18:09:49 -0300

    ✨ 08 enum

---
+++ StateType.php
@@ -0,0 +1,14 @@
+<?php
+enum StateType: string
+{
+    case READY = 'Ready';
+    case WAITING = 'Waiting';
+
+    public function labelOrOtherName(): string
+    {
+        return match ($this) {
+            self::READY => 'READYYYYYY',
+            self::WAITING => 'WAITINGGGGGG'
+        };
+    }
+}
